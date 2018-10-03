dc38d7e9950bfece9d7a402493f6a30f003fa3a6
Craig Sylvester <sylvestercraig72@gmail.com>
2018-10-03 11:37:56 -0500

    created footer, header, layout, style and navigation templates

--- index.js
+++ index.js
@@ -9,10 +9,7 @@ app.set('views' , "./views")
 app.set('view engine' , 'pug')
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/about', (req,res) => res.send("This Is The about section"))
-app.get('/about', (req,res) => res.send("welcome To Page 1"))
-app.get ('/home', (req, res) =>
-res.render('home',{title: "home", message:"welcome to page one"}))
+app.get('/', (req, res) => res.render('home'), {title: "Drone Racing"})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
